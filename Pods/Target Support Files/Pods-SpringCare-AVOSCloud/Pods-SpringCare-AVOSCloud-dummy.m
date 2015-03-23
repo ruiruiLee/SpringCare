4aa8ad393d7b0c28d3ef9cd2667ed903f604484c
LiuZach <24520575@qq.com>
2015-03-23 18:42:52 +0800

    no message

---
+++ Pods/Target Support Files/Pods-SpringCare-AVOSCloud/Pods-SpringCare-AVOSCloud-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpringCare_AVOSCloud : NSObject
+@end
+@implementation PodsDummy_Pods_SpringCare_AVOSCloud
+@end
---
+++ Pods/Target Support Files/Pods-SpringCare-AVOSCloudSNS/Pods-SpringCare-AVOSCloudSNS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpringCare_AVOSCloudSNS : NSObject
+@end
+@implementation PodsDummy_Pods_SpringCare_AVOSCloudSNS
+@end
